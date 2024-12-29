96dd2189e6f7e60e1aa3f7f18e6060066ff8fb99
Fukuta-Y <yufukuta1008@gmail.co.jp>
2024-12-29 18:55:07 +0900

    初回コミット

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // EditText, Button, TextViewの参照を取得
+        val inputText: EditText = findViewById(R.id.inputText)
+        val submitButton: Button = findViewById(R.id.submitButton)
+        val outputText: TextView = findViewById(R.id.outputText)
+
+        // ボタンのクリックリスナーを設定
+        submitButton.setOnClickListener {
+            // 入力されたテキストを取得
+            val userInput = inputText.text.toString()
+
+            // 入力されたテキストをTextViewに表示
+            outputText.text = "You entered: $userInput"
+        }
+    }
+}
